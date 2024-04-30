232623782129e386607df9c3fd771c48b40c7eb4
Gabriel Paranhos <biel.ferreira8@gmail.com>
2024-04-30 17:30:33 -0300

    feat: create type for response product

---
+++ products/types/response_types.go
@@ -0,0 +1,8 @@
+package products
+
+type ProductResponse struct {
+	Id			int		`json:"_id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float64 `json:"price"`
+}
