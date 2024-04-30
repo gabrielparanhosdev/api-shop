a8de59b3aa9549325987a57a4da47a1d0676cf7c
Gabriel Paranhos <biel.ferreira8@gmail.com>
2024-04-30 17:32:40 -0300

    fix: adjust spaces

--- products/types/response_types.go
+++ products/types/response_types.go
@@ -1,7 +1,7 @@
 package products
 
 type ProductResponse struct {
-	Id			int		`json:"_id"`
+	Id          int     `json:"_id"`
 	Name        string  `json:"name"`
 	Description string  `json:"description"`
 	Price       float64 `json:"price"`
