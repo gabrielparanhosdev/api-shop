ec9491789bd15c9406751e036018526c4dc098a8
Gabriel Paranhos <biel.ferreira8@gmail.com>
2024-05-01 12:46:16 -0300

    feat: create function insert new product in databse

--- helpers/database.go
+++ helpers/database.go
@@ -7,17 +7,50 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+const (
+	productsTable string = "products"
+)
+
+var log = echo.New()
+
 func GetProductDB(id int) (*types.ProductResponse, error) {
-	log := echo.New()
-	query := "SELECT _id, name, description, price FROM products WHERE _id = ?"
+	query := "SELECT _id, name, description, price FROM " + productsTable + " WHERE _id = ?"
 
 	var product types.ProductResponse
-	
+
 	err := db.DB.QueryRow(query, id).Scan(&product.Id, &product.Name, &product.Description, &product.Price)
-    if err != nil {
+	if err != nil {
 		log.Logger.Errorf("Error query select product", err)
-        return nil, err
-    }
+		return nil, err
+	}
 
 	return &product, nil
+}
+
+func InsertProduct(product *types.ProductRequest) (*types.ProductResponse, error) {
+
+	query := "INSERT INTO " + productsTable + " (name, description, price) VALUES (?, ?, ?)"
+
+	result, err := db.DB.Exec(query, product.Name, product.Description, product.Price)
+
+	if err != nil {
+		log.Logger.Errorf("Error query insert product", err)
+		return nil, err
+	}
+
+	productID, err := result.LastInsertId()
+
+	insertedProduct := &types.ProductResponse{
+		Name:        product.Name,
+		Description: product.Description,
+		Price:       product.Price,
+	}
+
+	if err == nil {
+		insertedProduct.Id = int(productID)
+	} else {
+		log.Logger.Errorf("Error get insert product", err)
+	}
+
+	return insertedProduct, nil
 }
