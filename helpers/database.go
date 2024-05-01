f91abd957800ff56a2cd12f668c1932357efa47a
Gabriel Paranhos <biel.ferreira8@gmail.com>
2024-05-01 12:47:44 -0300

    feat: create function update product in databse

--- helpers/database.go
+++ helpers/database.go
@@ -3,6 +3,7 @@ package helpers
 import (
 	"eulabs/common"
 	"eulabs/types"
+	"fmt"
 
 	"github.com/labstack/echo/v4"
 )
@@ -53,4 +54,34 @@ func InsertProduct(product *types.ProductRequest) (*types.ProductResponse, error
 	}
 
 	return insertedProduct, nil
+}
+
+
+func UpdateProductDB(id int, product *types.ProductRequest) (*types.ProductResponse, error) {
+
+	query := "UPDATE products SET name = ?, description = ?, price = ? WHERE _id = ?"
+
+    result, err := db.DB.Exec(query, product.Name, product.Description, product.Price, id)
+    if err != nil {
+        return nil, err
+    }
+
+    rowsAffected, err := result.RowsAffected()
+    if err != nil {
+        return nil, err
+    }
+    if rowsAffected == 0 {
+		msg := "dont update product"
+		log.Logger.Errorf(msg)
+        return nil, fmt.Errorf(msg)
+    }
+
+    updatedProduct := &types.ProductResponse{
+        Id:          id,
+        Name:        product.Name,
+        Description: product.Description,
+        Price:       product.Price,
+    }
+
+    return updatedProduct, nil
 }
